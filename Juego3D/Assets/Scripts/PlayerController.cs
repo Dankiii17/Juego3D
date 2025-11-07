6afc49aade4b6b5d6ec94c06a6cc430b7e9a2730
dmoyavilch <dmoyavilch1@educacion.navarra.es>
2025-11-07 18:21:58 +0100

    Limpieza de archivos temporales y actualizacion .gitignore

---
+++ Juego3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float gravityScale;
+    public CharacterController playerController;
+
+
+    private Vector3 moveDirection;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float ystore = moveDirection.y;
+
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * moveSpeed;
+
+        moveDirection.y = ystore;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        playerController.Move(moveDirection * Time.deltaTime);
+    }
+}
